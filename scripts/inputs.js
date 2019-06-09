1736d979d14787abbe284da20bcfbe8f653485d5
Josh <lee.josh93@gmail.com>
2019-06-09 12:54:03 -0500

    Created input boxes and loaded mad lib story.

---
+++ scripts/inputs.js
@@ -0,0 +1,25 @@
+
+var app = angular.module("inputs", []);
+
+app.controller("inputCtrl", function($scope) {
+  $scope.adjective1 = "";
+  $scope.adjective2 = "";
+  $scope.adjective3 = "";
+  $scope.noun1 = "";
+  $scope.noun2 = "";
+  $scope.noun3 = "";
+  $scope.pluralNoun1 = "";
+  $scope.pluralNoun2 = "";
+  $scope.pluralNoun3 = "";
+  $scope.pluralNoun4 = "";
+  $scope.game = "";
+  $scope.verbEndingInIng1 = "";
+  $scope.verbEndingInIng2 = "";
+  $scope.verbEndingInIng3 = "";
+  $scope.verbEndingInIng4 = "";
+  $scope.plant = "";
+  $scope.partOfTheBody = "";
+  $scope.place = "";
+  $scope.number = "";
+});
+
